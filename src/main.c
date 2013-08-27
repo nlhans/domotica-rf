ce732e045f74f3a7fd71173c192a9f03963b4c55
Hans <hans1990@gmail.com>
2013-08-27 17:42:10 +0200

    PIC16F1508 sensor code

--- src/main.c
+++ src/main.c
@@ -26,25 +26,19 @@ _FICD(ICS_PGD2 & JTAGEN_OFF)
 #endif
 #else
 #define TRANSMITTER
-        
+
+ #define _XTAL_FREQ 16000000
+
  __CONFIG( FOSC_INTOSC & WDTE_OFF & PWRTE_ON & MCLRE_ON & CP_OFF & BOREN_OFF & CLKOUTEN_OFF & IESO_OFF & FCMEN_OFF  );
  __CONFIG( WRT_ALL & STVREN_ON & BORV_HI & LPBOR_OFF & LVP_OFF );
+
+#include <xc.h>
 
 #endif
 
 
-typedef struct
-{
-    unsigned PacketReady		: 1;
-    unsigned ResetRadio		: 1;
-} TFlags;
-
-char PacketLen;
-TFlags Flags;
-
-char PackState;
-char powerState;
-
+ const UI16_t humids30c[15] = {65535, 39000, 20000, 9800, 4700, 1310, 770, 440, 250, 170, 105, 72, 50, 36, 25 };
+ 
 void initRFPorts(void)
 {
     TRIS_SPI_SDI 	= INPUT_PIN;
@@ -75,22 +69,155 @@ void UartInit()
 #endif
 }
 
+void TimInit(void)
+{
+    // 16MHz/4 Fclk
+    // -> 2kHz
+    OPTION_REG = 0b00000011;
+    // div by 16 -> 125 tmr value
+    TMR0 = 255-125;
+
+    INTCONbits.TMR0IE = 1;
+    INTCONbits.TMR0IF = 0;
+    //INTCONbits.GIE = 1;
+}
+
+#define SENSOR_PWR LATCbits.LATC2
+
+int enableHumidPower = 0;
+
+void interrupt isr()
+{
+    // Timer 0
+    INTCONbits.TMR0IF = 0;
+    if(enableHumidPower == 0)
+    {
+        SENSOR_PWR = 0;
+    }
+    else
+    {
+        if (SENSOR_PWR != 0)
+        {
+            SENSOR_PWR = 0;
+        }
+        else
+        {
+            SENSOR_PWR = 1;
+        }
+    }
+    TMR0 = 255-125;
+}
+
+void AdcInit(void)
+{
+    // PIC16
+    // 16M -> Fosc/64 = 4us
+    // 1M -> Fosc/4 = 4us
+
+    FVRCON = 0b11100001;
+    ADCON2 = 0b00000000;
+    ADCON1 = 0b11110000;
+    ADCON0 = 0b00000000;
+
+    ADCON0 = 0b00010011;
+}
+
 UI08_t buffer[16];
 
+#include "math.h"
+void AdcConvert(void)
+{
+    UI16_t temp;
+    UI16_t sensor;
+    
+    //LATC |= 1<<2;
+
+    //enableHumidPower = 1;
+
+    SENSOR_PWR = 1;
+    ADCON0 = 0b00010011;
+    __delay_ms(100);
+    temp = (ADRESH << 8) | ADRESL;
+    ADCON0 = 0b00010011;
+    while ((ADCON0 & 0x2));
+    temp += (ADRESH << 8) | ADRESL;
+    SENSOR_PWR = 0;
+
+    sensor=temp / 2;
+
+    buffer[0] = ADRESH;
+    buffer[1] = ADRESL;
+
+    ADCON0 = 0b01111111;
+    __delay_us(200);
+    while (ADCON0 & 0x2);
+    ADCON0 = 0b01111111;
+    while (ADCON0 & 0x2);
+
+    buffer[2] = ADRESH;
+    buffer[3] = ADRESL;
+    temp = (ADRESH << 8) | ADRESL;
+
+    ADCON0 = 0b01110111;
+    while (ADCON0 & 0x2);
+    __delay_us(200);
+    ADCON0 = 0b01110111;
+    while (ADCON0 & 0x2);
+
+    temp = (ADRESH << 8) | ADRESL;
+    temp = (temp - 480) * 100 / 120 - 43;
+    temp = 29;
+    buffer[4] = ADRESH;
+    buffer[5] = ADRESL;
+    buffer[6] = temp;
+
+    float voltage = (sensor/1023.0);
+    float ntcResistance = 47*pow(1/1.044, buffer[6]-25);
+    float humidityResistance = (1/(1/voltage - 1)) * ntcResistance * 10;
+
+    UI16_t iHumidityResistance = (UI16_t) humidityResistance;
+    buffer[7] = iHumidityResistance >> 8;
+    buffer[8] = iHumidityResistance & 0xFF;
+
+    UI08_t ii = 0;
+
+    while(ii < 14)
+    {
+        if (humids30c[ii+1] <= iHumidityResistance && humids30c[ii] >= ii)
+        {
+            // GOOD
+            float part = (humids30c[ii] - iHumidityResistance) / ((float)humids30c[ii+1] - humids30c[ii]);
+            float humidity = part*5 + 20 + ii * 5;
+            buffer[9] = (UI08_t) humidity;
+            break;
+        }
+        ii++;
+    }
+}
+
 int main ( void ){
-    UI08_t i, j;
+    UI08_t i, j,k;
     #ifdef PIC24_HW
         #if defined(__PIC24FJ64GB004__)
         AD1PCFG = 0xFFFF;
         #else
         ADPCFG = 0xFFFF; // Ports as digital, not analog
         #endif
-#else
+    #else
         OSCCON = 0b01111000;
-    ANSELA = 0;
-    ANSELB = 0;
-    ANSELC = 0;
+        
+        TRISC |= 1<<0; // Humidity
+        TRISC &= ~(1<<2);
+        TRISA &= ~(1<<0);
+        TRISA &= ~(1<<1);
+        
+        ANSELA = 0;
+        ANSELB = 0;
+        ANSELC = (1<<0); // RC0, AN4
     #endif
+
+    TimInit();
+    AdcInit();
 
     UartInit();
     initRFPorts();
@@ -98,8 +225,9 @@ int main ( void ){
     RF_POWER = 1;
 
     MRF49XA_Init();
-    printf("Hi\r\n");
-    Mrf49XaDelay(1);
+    //printf("Hi\r\n");
+    Mrf49XaDelay(2);
+    Mrf49XaDelay(2);
     
     MRF49XA_Reset_Radio();
 
@@ -107,18 +235,18 @@ int main ( void ){
     {
         #ifdef TRANSMITTER
 
-        buffer[0] = 0x12;
-        buffer[1] = 0x34;
-        buffer[2] = 0x56;
-        buffer[3] = 0x78;
-        buffer[4] = 0x90;
-        buffer[5] = 0xAB;
-        buffer[6] = 0xCD;
-        buffer[7] = 0xEF;
+        PORTA |= 1<<0;
+        AdcConvert();
+        PORTA &= ~(1<<0);
 
-        MRF49XA_TxPacket(buffer, 8);
+        PORTA |= 1<<1;
         
-        printf("Tx\r\n");
+        MRF49XA_TxPacket(buffer, 10);
+        PORTA &= ~(1<<1);
+        for(k=0;k<30;k++)
+            Mrf49XaDelay(2);
+
+        //printf("Tx\r\n");
         
         #else
 
